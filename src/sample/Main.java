e9fb4c35b6e718fbb75b77599c407e3fb97d4846
Carlos-Montemayor <carlos.a.montemayor.01@gmail.com>
2019-11-17 16:06:28 -0500

    worked out how to properly format the window size and finished the main window design

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,10 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,10 +14,43 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        Bounds rootBounds = root.getBoundsInLocal();
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, root.getLayoutX(), root.getLayoutY()));
+        Scene worldScene = new Scene(root, rootBounds.getWidth(), rootBounds.getHeight());
+        primaryStage.setScene(worldScene);
+        //primaryStage.setResizable(false);
         primaryStage.show();
+        fixBounds(primaryStage, root);
+    }
+
+    private void fixBounds(Stage stage, Parent root) {
+        double deltaW = stage.getWidth() - stage.getWidth();
+        double deltaH = stage.getHeight() - stage.getHeight();
+
+        Bounds prefBounds = getPrefBounds(root);
+
+        stage.setMinWidth(prefBounds.getWidth() + deltaW);
+        stage.setMinHeight(prefBounds.getHeight() + deltaH);
+    }
+
+    private Bounds getPrefBounds(Node node) {
+        double prefWidth ;
+        double prefHeight ;
+
+        Orientation bias = node.getContentBias();
+        if (bias == Orientation.HORIZONTAL) {
+            prefWidth = node.prefWidth(-1);
+            prefHeight = node.prefHeight(prefWidth);
+        } else if (bias == Orientation.VERTICAL) {
+            prefHeight = node.prefHeight(-1);
+            prefWidth = node.prefWidth(prefHeight);
+        } else {
+            prefWidth = node.prefWidth(-1);
+            prefHeight = node.prefHeight(-1);
+        }
+        return new BoundingBox(0, 0, prefWidth, prefHeight);
     }
 
 
--- src/sample/WindowController.java
+++ src/sample/WindowController.java
@@ -2,11 +2,22 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.image.BufferedImage;
 
 public class WindowController {
     public Label helloWorld;
+    public ImageView field;
 
     public void sayHelloWorld(ActionEvent actionEvent) {
+
         helloWorld.setText("Hello World!");
+    }
+
+    public void updateField() {
+
+        // TODO: figure out how to make JavaFX Images to pass to the ImageView
     }
 }
